19ecc7ba3ae20b73bf8aa0517ef5792234d4f532
FrederickPerazzelli <81708400+FrederickPerazzelli@users.noreply.github.com>
2021-04-01 10:49:31 -0400

    Update apple.h

--- apple.h
+++ apple.h
@@ -1,17 +1,43 @@
-#include "point.h"
-#include <iostream>
-#include <assert.h>
-#include <windows.h>
+/* En-tête de programme
+ ==========================================
+Programme:		point.h
+Acteur:			Frederick Perazzelli-Delorme et Nicolas Garant
+Date de création:	
+But du programme:	Méthodes et prototypes de l'objet pomme;
+====================================================== */
 
-class apple {
-private:
-    point _apple; //position de la pomme
-public:
-    apple();                                //initialise la positon à 0 et couleur rouge
-    apple(int x, int y);                    //initialise la position reçue
-    void setPosition(int x, int y);         //initialise la pomme à la position reçue
-    const point &getPosition() const;       //retourne la position de la pomme
-    void draw(ostream &sortie) const;       //draw la pomme rouge
+/* directive au pré-processeur
+-------------------------------*/
+#pragma once
+
+#include<iostream>
+#include"point.h"
+#include"rectangle.h"
+
+class pomme {
+
+	private:
+
+		point _pomme;										//	Objet pomme;
+
+	public:
+
+		//	Constructeur;
+		pomme();											//	Constructeur par défaut;
+		pomme(int x, int y);								//	Constructeur avec variable;
+
+		//	Destructeur;
+		~pomme();											//	Destructeur de l'objet pomme;
+
+		//	Getteur;
+		const point& getPosition();							//	Getteur de la position de pomme;
+
+		//	Setteur;	
+		void setPosition(int x, int y);						//	Setteur de la position de la pomme;
+
+		//	Autre Méthode;
+		void draw(ofstream& output) const;					//	Dessine la pomme;
 };
 
-ostream &operator<<(ostream &sortie, const apple &a);
+//	Operator;
+ostream& operator<<(ostream& output, const pomme& p);		//	Operatour cout / output;
