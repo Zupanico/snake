7c8eef9d82704f3c46c5a4335c92056ac9573f12
FrederickPerazzelli <81708400+FrederickPerazzelli@users.noreply.github.com>
2021-04-01 10:50:19 -0400

    Update apple.h

--- apple.h
+++ apple.h
@@ -18,26 +18,26 @@ class pomme {
 
 	private:
 
-		point _pomme;										//	Objet pomme;
+		point _pomme;					//	Objet pomme;
 
 	public:
 
 		//	Constructeur;
-		pomme();											//	Constructeur par défaut;
-		pomme(int x, int y);								//	Constructeur avec variable;
+		pomme();					//	Constructeur par défaut;
+		pomme(int x, int y);				//	Constructeur avec variable;
 
 		//	Destructeur;
-		~pomme();											//	Destructeur de l'objet pomme;
+		~pomme();					//	Destructeur de l'objet pomme;
 
 		//	Getteur;
-		const point& getPosition();							//	Getteur de la position de pomme;
+		const point& getPosition();			//	Getteur de la position de pomme;
 
 		//	Setteur;	
-		void setPosition(int x, int y);						//	Setteur de la position de la pomme;
+		void setPosition(int x, int y);			//	Setteur de la position de la pomme;
 
 		//	Autre Méthode;
-		void draw(ofstream& output) const;					//	Dessine la pomme;
-};
+		void draw(ofstream& output) const;		//	Dessine la pomme;
+
 
 //	Operator;
 ostream& operator<<(ostream& output, const pomme& p);		//	Operatour cout / output;
