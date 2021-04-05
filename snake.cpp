1d873ad3272326de8a5632f3b95d7f8cac05ff91
FrederickPerazzelli <81708400+FrederickPerazzelli@users.noreply.github.com>
2021-04-05 11:03:45 -0400

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,53 +1,143 @@
-/************************************************************************************
-* Auteur : Nicolas Garant *
-* Nom : snake.h *
-* Date : 30 mars 2021 *
-* Description : L'objet snake implémenté avec un tableau primitif de point et une *
-* taille pouvant se déplacer pour être intégré dans le jeu snake *
-************************************************************************************/
+/* En-tête de programme
+ ==========================================
+Programme:		point.h
+Acteur:			Frederick Perazzelli-Delorme et Nicolas Garant
+Date de création:
+But du programme:	
+====================================================== */
 
+/* directive au pré-processeur
+-------------------------------*/
 #include "snake.h"
 
 
-snake::snake() {
-    _taille = 6;
+//	Constructeur du serpent par défaut
+snake::snake()
+{
+	_snake[0].setPosition(20, 7);
+	_snake[1].setPosition(21, 7);
+	_snake[2].setPosition(22, 7);
+	_snake[3].setPosition(23, 7);
+	_snake[4].setPosition(24, 7);
+	_snake[5].setPosition(25, 7);
+	_taille = 6;
 }
 
-snake::snake(int x, int y) {
-
+// constructeur du serpent en recevant;
+snake::snake(int x, int y)
+{
+	_snake[0].setPosition(x, y);
+	_snake[1].setPosition(x + 1, y);
+	_snake[2].setPosition(x + 2, y);
+	_snake[3].setPosition(x + 3, y);
+	_snake[4].setPosition(x + 4, y);
+	_snake[5].setPosition(x + 5, y);
+	_taille = 6;
 }
 
-void snake::initialize(int x, int y) {
-
+//	Destructeur du snake
+snake::~snake()
+{
+	_snake[0].setPosition(0, 0);
+	_taille = 0;
 }
 
-
-point snake::newPosition(int dir) {
-    point newHead = _snake[0];
-
-    switch (dir) {
-        case 1: //gauche
-            newHead.setX(newHead.getX() - 1);
-            break;
-        case 2: //droite
-            newHead.setX(newHead.getX() + 1);
-            break;
-        case 3:
-            newHead.setY(newHead.getY() - 1);
-            break;
-        case 4:
-            newHead.setY(newHead.getY() + 1);
-    }
-    return newHead;
+//	Initialise le serpent
+void snake::initialize(int x, int y)
+{
+	snake(x, y);
 }
 
-void snake::move(int dir) {
-    point newHead = newPosition(dir);
-    for (int i = _taille - 1; i >= 0; --i) {
-        _snake[i + 1] = _snake[i];
-    }
-    _snake[0] = newHead;
+// Renvoie la tête du serpent
+const point& snake::getHeadPosition() const
+{
+	return _snake[0];
 }
 
+// renvoie la position du serpent
+const point& snake::getPosition(int ind) const
+{
+	return _snake[ind];
+}
+
+// operator pour aller chercher la position du serpent
+const point& snake::operator[](int ind) const
+{
+	return getPosition(ind);
+}
+
+//	Renvoie la taille du serpent
+int snake::getTaille() const
+{
+	return _taille;
+}
+
+//	Donne la nouvelle position de la tête du serpent;
+point snake::nouvellePosition(int dir)
+{
+	point newHead = _snake[0];
+
+	switch (dir) {
+	case 1: 
+		newHead.setX(newHead.getX() - 1);
+		break;
+	case 2: 
+		newHead.setX(newHead.getX() + 1);
+		break;
+	case 3:
+		newHead.setY(newHead.getY() - 1);
+		break;
+	case 4:
+		newHead.setY(newHead.getY() + 1);
+	}
+	return newHead;
+}
+
+//	Méthode collision du serpent;
+bool snake::ifCollision(const point& pos) const
+{
+	for (int i = 0; i < _taille; i++) {
+
+		if (pos == _snake[i]) {
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+//	Fait bouger le serpent;
+void snake::move(int dir)
+{
+	point newHead = nouvellePosition(dir);
+
+	for (int i = _taille - 1; i >= 0; --i) {
+		_snake[i + 1] = _snake[i];
+	}
+	_snake[0] = newHead;
+}
+
+void snake::eat(int dir)
+{
+	move(dir);
+	_taille++;
+}
+
+//	Operator draw;
+void snake::draw(ostream& output) const
+{
+	for (int i = 0; i < _taille; i++) {
+
+		gotoxy(_snake[i].getX(), _snake[i].getY());
+		output << "\xEF";
+	}
+}
+
+//	Operator cout;
+ostream& operator<<(ostream& output, const snake& s)
+{
+	return output;
+}
 
 
