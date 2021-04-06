74b4419f68964f3abdd7d638bd30c1b3c683eea5
FrederickPerazzelli <81708400+FrederickPerazzelli@users.noreply.github.com>
2021-04-05 22:50:22 -0400

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -9,10 +9,13 @@ But du programme:
 /* directive au pré-processeur
 -------------------------------*/
 #include "snake.h"
-
+#include "point.h"
 
 //	Constructeur du serpent par défaut
-snake::snake() {
+
+
+snake::snake()
+{
     _snake[0].setPosition(20, 7);
     _snake[1].setPosition(21, 7);
     _snake[2].setPosition(22, 7);
@@ -40,21 +43,21 @@ snake::~snake() {
 
 //	Initialise le serpent
 void snake::initialize(int x, int y) {
-    snake s(x, y);
+    snake(x, y);
 }
 
 // Renvoie la tête du serpent
-const point &snake::getHeadPosition() const {
+const point& snake::getHeadPosition() const {
     return _snake[0];
 }
 
 // renvoie la position du serpent
-const point &snake::getPosition(int ind) const {
+const point& snake::getPosition(int ind) const {
     return _snake[ind];
 }
 
 // operator pour aller chercher la position du serpent
-const point &snake::operator[](int ind) const {
+const point& snake::operator[](int ind) const {
     return getPosition(ind);
 }
 
@@ -68,52 +71,57 @@ point snake::nouvellePosition(int dir) {
     point newHead = _snake[0];
 
     switch (dir) {
-        case 1:
-            newHead.setX(newHead.getX() - 1);
-            break;
-        case 2:
-            newHead.setX(newHead.getX() + 1);
-            break;
-        case 3:
-            newHead.setY(newHead.getY() - 1);
-            break;
-        case 4:
-            newHead.setY(newHead.getY() + 1);
-            break;
+    case 1:
+        newHead.setX(newHead.getX() - 1);
+        break;
+    case 2:
+        newHead.setX(newHead.getX() + 1);
+        break;
+    case 3:
+        newHead.setY(newHead.getY() - 1);
+        break;
+    case 4:
+        newHead.setY(newHead.getY() + 1);
+        break;
     }
     return newHead;
 }
 
 //	Méthode collision du serpent;
-bool snake::ifCollision(const point &pos) const {
+bool snake::ifCollision(const point& pos) const {
+    
     for (int i = 0; i < _taille; i++) {
+
         if (pos == _snake[i]) {
             return true;
         }
     }
+
     return false;
 }
 
 //	Fait bouger le serpent;
 void snake::move(int dir) {
+
     point newHead = nouvellePosition(dir);
 
     for (int i = _taille - 1; i >= 0; --i) {
         _snake[i].setColor(6);
         _snake[i + 1] = _snake[i];
-
     }
     _snake[0] = newHead;
     _snake[_taille].setColor(0);
 }
 
 void snake::eat(int dir) {
+
     move(dir);
     _taille++;
 }
 
 //	Operator draw;
-void snake::draw(ostream &output) const {
+void snake::draw(ostream& output) const {
+    
     for (int i = 0; i < _taille; i++) {
         gotoxy(_snake[i].getX(), _snake[i].getY());
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
@@ -125,6 +133,6 @@ void snake::draw(ostream &output) const {
 }
 
 //	Operator cout;
-ostream &operator<<(ostream &output, const snake &s) {
+ostream& operator<<(ostream& output, const snake& s) {
     return output;
 }
