6f8e1c652aa60afdb67e9d68fa27389cd5f23231
FrederickPerazzelli <81708400+FrederickPerazzelli@users.noreply.github.com>
2021-04-05 22:47:18 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,12 +1,22 @@
-//
-// Created by nicol on 31/mars/2021.
-//
+/* En-tête de programme
+ ==========================================
+Programme:		point.h
+Acteur:			Frederick Perazzelli-Delorme et Nicolas Garant
+Date de création:
+But du programme:	
+====================================================== */
 
-#include "game.h"
-
+/* directive au pré-processeur
+-------------------------------*/
+#include"point.h"
+#include"game.h"
+#include"rectangle.h"
+#include"pomme.h"
+#include"snake.h"
 
 void game::play() {
     //Variables
+   
     point pos;
 
     initialize();
@@ -15,27 +25,33 @@ void game::play() {
         printScore(cout);
         printLive(cout);
         inputKey();
-
+       
         if (_dir != STOP) {
+
             pos = _ekans.nouvellePosition(_dir);
-            if (!canMove(pos) || _ekans.ifCollision(pos)) {
+            _ekans.move(_dir);
+
+            if (!canMove(pos)) {
+
+                _cptLive--;  
                 drawScreen(cout);
-                _cptLive--;
-                printLive(cout);
+               
                 if (_cptLive == 0) {
                     _lose = true;
                 }
-            } else if (pos == _pomme.getPosition()) {
+            }
+            else if (pos == _pomme.getPosition()) {
+
                 _ekans.eat(_dir);
                 _score++;
                 printScore(cout);
                 createApple();
-            } else {
-                _ekans.nouvellePosition(_dir);
-                _ekans.move(_dir);
             }
+            else {
+                _ekans.nouvellePosition(_dir);
+            }
+
             _ekans.draw(cout);
-            _pomme.draw(cout);
             Sleep(100);
         }
     }
@@ -44,27 +60,30 @@ void game::play() {
 
 //Touches entrées, initialise la direction et le booléen _lose si Q est touché
 void game::inputKey() {
+   
     int touche;
-    if (_kbhit()) {                 //si une touche est appuyée
-        touche = _getch();          //saisie la touche
-        if (touche == 'q') {        //si la touche est q, on veut arrêter le jeu
+    
+    if (_kbhit()) {                     //si une touche est appuyée
+        touche = _getch();              //saisie la touche
+        if (touche == 'q') {            //si la touche est q, on veut arrêter le jeu
             _lose = true;
             _dir = STOP;
-        } else if (touche == 224) { //si la touche est 224, c’est une flèche
-            touche = _getch();      //dans le buffer on prend la 2e partie de la touche
+        }
+        else if (touche == 224) {       //si la touche est 224, c’est une flèche
+            touche = _getch();          //dans le buffer on prend la 2e partie de la touche
             switch (touche) {
-                case 75:            //code ascii des flèches
-                    _dir = LEFT;
-                    break;
-                case 72:
-                    _dir = UP;
-                    break;
-                case 80:
-                    _dir = DOWN;
-                    break;
-                case 77:
-                    _dir = RIGHT;
-                    break;
+            case 75:                    //code ascii des flèches
+                _dir = LEFT;
+                break;
+            case 72:
+                _dir = UP;
+                break;
+            case 80:
+                _dir = DOWN;
+                break;
+            case 77:
+                _dir = RIGHT;
+                break;
             }
 
         }
@@ -74,10 +93,10 @@ void game::inputKey() {
 //initialise les types primitifs à 0
 game::game() {
     _lose = false;
-    _dir = NONE;
+    _dir = LEFT;
     _cptLive = 3;
     _score = 0;
-    _ekans.initialize(20, 7);
+    _ekans.initialize(_width/2, _height/2);
     _plateau.setLargeur(_width);
     _plateau.setHauteur(_height);
 }
@@ -90,37 +109,29 @@ game::~game() {
     _score = 0;
     _ekans.initialize(0, 0);
     _plateau.setLargeur(0);
-    _plateau.setHauteur(0);
+    _plateau.setHauteur(0);  
 }
 
 //initalise le jeu
 void game::initialize() {
-    _lose = false;
-    _dir = STOP;
-    _cptLive = 3;
-    _score = 0;
-    _ekans.initialize(20, 7);
-    _plateau.setLargeur(_width);
-    _plateau.setHauteur(_height);
-    _plateau.draw(cout);
-    createApple();
-    //game();
-    //drawScreen(cout);
+    game();
+    drawScreen(cout);
 }
 
 //génère une nouvelle position aléatoire ds le terrain
 point game::randPosition() const {
+    
     point p;
     int x, y;
-    srand(time(NULL));
+    srand (time(NULL));
 
-    x = rand() % 37 + 2;    //Position random
+    x = rand() % 38 + 2;    //Position random
     y = rand() % 17 + 2;
 
     //Vérification que le rand n'est pas la position du snake
     for (int i = 0; i < _ekans.getTaille(); ++i) {
         if (x == _ekans[i].getX() && y == _ekans[i].getY()) {
-            x = rand() % 37 + 2;    //Position random
+            x = rand() % 38 + 2;    //Position random
             y = rand() % 17 + 2;
         }
     }
@@ -129,46 +140,65 @@ point game::randPosition() const {
     return p;
 }
 
+//  Creation de la pomme;
 void game::createApple() {
     _pomme.setPosition(randPosition().getX(), randPosition().getY());
+    _pomme.draw(cout);
 }
 
-bool game::canMove(const point &p) const {
-    if ((_ekans[0].getX() == 0 || _ekans[0].getX() == 39) || (_ekans[0].getY() == 0 || _ekans[0].getY() == 19)) {
-        return false;
+//  Peu tu bouger;
+bool game::canMove(const point& p) const {
+   
+    if (_ekans.ifCollision(p)) {
+
+        if ((_ekans[0].getX() == 0 || _ekans[0].getX() == 39) || (_ekans[0].getY() == 0 || _ekans[0].getY() == 19)) {
+
+            return false;
+        }
+        for (int i = 1; i < _ekans.getTaille(); i++) {
+
+            if (_ekans[0] == _ekans[i]) {
+
+                return false;
+            }
+        }
     }
     return true;
 }
 
 
+// renvoie le score
 int game::getScore() const {
     return _score;
 }
 
-void game::printScore(ostream &output) const {
-    gotoxy(0, 22);
+// imprime le score
+void game::printScore(ostream& output) const {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-    output << endl << "Score : " << _score << endl;
-}
-
-void game::printLive(ostream &output) const {
     gotoxy(0, 23);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-    output << endl << "Vies restantes : " << _cptLive << endl;
+    output << "Score : " << _score << endl;
 }
 
-void game::printEndGame(ostream &ouput) const {
+// imprime le nombre de vie
+void game::printLive(ostream& output) const {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
     gotoxy(0, 24);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-    ouput << endl << "Game Over" << endl << "Score : " << _score;
+    output << "Vies restantes : " << _cptLive << endl;
 }
 
-void game::drawScreen(ostream &output) {
+// imprime game over
+void game::printEndGame(ostream& ouput) const {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    gotoxy(0, 26);
+    ouput << "Game Over" << endl << "Score : " << _score << endl;
+}
+
+void game::drawScreen(ostream& output)
+{
     system("cls");
     printScore(output);
-    printLive((output));
-    _ekans.initialize(20, 7);
-    _dir = NONE;
+    printLive(output);
+    _ekans.initialize(_width / 2, _height / 2);
     _plateau.draw(output);
     _ekans.draw(output);
     createApple();
