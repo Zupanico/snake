242f2225cb2b9ff2751d39b45ff2af078c64edef
FrederickPerazzelli <81708400+FrederickPerazzelli@users.noreply.github.com>
2021-04-05 22:49:15 -0400

    Update game.h

--- game.h
+++ game.h
@@ -1,52 +1,67 @@
-/************************************************************************************
-* Auteur : Nicolas Garant *
-* Nom : game.h *
-* Date : 30 mars 2021 *
-* Description : L'objet game implémente Le jeu snake en console où les flèches *
-* servent à déplacer le snake dans l’écran pour manger les pommes, *
-* mais attention de ne pas toucher les côté ou le snake lui-même *
-*************************************************************************************/
-//Directives aux préprocesseurs
-#pragma once
+/* En-tête de programme
+ ==========================================
+Programme:		point.h
+Acteur:			Frederick Perazzelli-Delorme et Nicolas Garant
+Date de création:
+But du programme:	Méthodes et prototypes de l'objet game;
+====================================================== */
 
-#include <iostream>
-#include <conio.h>
-#include "rectangle.h"
-#include "game.h"
-#include "pomme.h"
-#include "snake.h"
-#include <windows.h>
-#include <time.h>
+/* directive au pré-processeur
+-------------------------------*/
+#pragma	once
+#include<iostream>
+#include<conio.h>
+#include"rectangle.h"
+#include"snake.h"
+#include"pomme.h"
+#include"point.h"
+#include<windows.h>
+#include<time.h>
 
-using namespace std;
+using	namespace std;
 
 class game {
-private:
-    bool _lose;             //Si on perd
-    int _dir,               //la direction choisie
-    _cptLive,               //le compteur de vie
-    _score;                 //le score de la game
-    snake _ekans;           //le snake
-    pomme _pomme;           //la pomme
-    rectangle _plateau;     //rectangle du terrain de jeu
-    const int _width = 40;  //dimension du terrain de jeu
-    const int _height = 20;
-    enum direction {
-        STOP, LEFT, RIGHT, UP, DOWN, NONE
-    };
-public:
-    game();                     //initialise les types primitifs à 0
-    ~game();                    //initialise les types primitifs à 0
-    void initialize();          //initalise le jeu
-    point randPosition() const; //génère une nouvelle position aléatoire ds le terrain
-    void createApple();         //génère une nouvelle pomme tq position est ds le snake
-    void play();                //la main loop du jeu
-    void inputKey();            //la saisie des touches pour le déplacement du snake
-    bool canMove(const point &p) const;//retourne vrai si la tête du snake peut bouger
-    int getScore() const;       //retourne le score (seule info qui pourrait être intéressante
-    //de l’extérieur) et aucun setteur pour cet objet.
-    void printScore(ostream &sortie) const;     //affiche le score
-    void printLive(ostream &output) const;      //affiche le compteur de vie
-    void printEndGame(ostream &ouput) const;    //affiche game over et le score
-    void drawScreen(ostream &output);           //Affiche tout les éléments à l'écran
+
+    private:
+        bool _lose;                 //Si on perd
+
+        int _dir,                   //la direction choisie
+            _cptLive,               //le compteur de vie
+            _score;                 //le score de la game
+
+        snake _ekans;               //le serpent
+        pomme _pomme;               //la pomme
+
+        rectangle _plateau;         //rectangle du terrain de jeu
+        const int _width = 40;      //dimension du terrain de jeu
+        const int _height = 20;     // Hauteur;
+
+        enum direction { STOP, LEFT, RIGHT, UP, DOWN, NONE };
+
+
+    public:
+
+        game();                     //initialise les types primitifs à 0
+
+        ~game();                    //initialise les types primitifs à 0
+
+        void initialize();          //initalise le jeu
+
+        point randPosition() const; //génère une nouvelle position aléatoire ds le terrain
+
+        void createApple();         //génère une nouvelle pomme tq position est ds le snake
+
+        void play();                //la main loop du jeu
+
+        void inputKey();            //la saisie des touches pour le déplacement du snake
+
+        bool canMove(const point& p) const; //retourne vrai si la tête du snake peut bouger
+
+        int getScore() const;       //retourne le score (seule info qui pourrait être intéressante
+                                    //de l’extérieur) et aucun setteur pour cet objet.
+
+        void printScore(ostream& output) const;     //affiche le score
+        void printLive(ostream& output) const;      //affiche le compteur de vie
+        void printEndGame(ostream& output) const;   //affiche game over et le score
+        void drawScreen(ostream& output);
 };
